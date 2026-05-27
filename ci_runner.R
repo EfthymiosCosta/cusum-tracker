15adfba0608b1e16a2ef7fe17efbef306559e474
EfthymiosCosta <efthymios.costa@gmail.com>
2026-05-27 19:35:08 +0100

    update runner

--- ci_runner.R
+++ ci_runner.R
@@ -3,12 +3,14 @@ source("R/cusum_funs.R")
 log_dir <- "cusum_logs"
 
 # Snapshot the logs directory before
-files_before <- list.files(log_dir, pattern = "\\.rds$", full.names = TRUE)
+files_before <- list.files(log_dir, pattern = "\\.rds$", full.names = TRUE,
+                           recursive = TRUE)
 mtimes_before <- setNames(file.mtime(files_before), files_before)
 
 source("run_tests.R")
 
-files_after <- list.files(log_dir, pattern = "\\.rds$", full.names = TRUE)
+files_after <- list.files(log_dir, pattern = "\\.rds$", full.names = TRUE,
+                          recursive = TRUE)
 mtimes_after <- setNames(file.mtime(files_after),  files_after)
 
 is_new <- !(files_after %in% files_before)
@@ -25,6 +27,9 @@ for (lf in touched) {
     nm <- if (!is.null(log$config$test_name) && nzchar(log$config$test_name))
       log$config$test_name
     else tools::file_path_sans_ext(basename(lf))
+    rel_dir <- dirname(sub(paste0("^", log_dir, "/?"), "", lf))
+    display <- if (rel_dir %in% c(".", "")) nm
+    else sprintf("%s / %s", gsub("_", " ", rel_dir), nm)
     signals[[nm]] <- list(S_t = last$S_t,
                           h = log$config$h,
                           p = last$p_value)
