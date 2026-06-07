35d6c7b87a7a2601bf46e43bf3c0205e2365fe84
EfthymiosCosta <efthymios.costa@gmail.com>
2026-06-07 11:22:37 +0100

    update runner logs

--- ci_runner.R
+++ ci_runner.R
@@ -1,4 +1,4 @@
 source("R/cusum_funs.R")
 library(testthat)
-options(cusum.dir = "cusum_logs")
+options(cusum.dir = normalizePath("cusum_logs", mustWork = FALSE))
 testthat::test_dir("tests", stop_on_failure = TRUE)
